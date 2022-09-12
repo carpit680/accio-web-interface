6f9ca63704e5aa9e46d23e69b387f0fbcde5290d
Arpit Chauhan <carpit680@gmail.com>
2022-09-12 19:45:56 +0530

    no ros node recreation on socket reconnect

--- server/index.js
+++ server/index.js
@@ -5,84 +5,75 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import startSimHandler from "./startSimHandler.js";
 import rclnodejs from "rclnodejs";
-import io from "socket.io-client";
 
 const app = express();
 const httpServer = createServer(app);
-const socketio = new Server(httpServer);
+const io = new Server(httpServer);
 const PORT = process.env.PORT || 3001;
-const socket = io();
 
 rclnodejs.init();
 
-var socket_connected = false;
-socketio.on("connection", (socket) => {
-	// if (!socket_connected) {
-	// 	socket_connected = true;
-		console.log("client connected: ", socket.id);
+io.on("connection", (socket) => {
+	console.log("client connected: ", socket.id);
 
-		const nodeSim = new rclnodejs.Node("sim_starter");
-		const nodeRobot = new rclnodejs.Node("robot_subscriber");
-		const nodePending = new rclnodejs.Node("pending_subscriber");
-		const nodeFulfilled = new rclnodejs.Node("fulfilled_subscriber");
-		const sim_pub = nodeSim.createPublisher(
-			"std_msgs/msg/Bool",
-			"start_simulation"
-		);
-		startSimHandler(socketio, socket, sim_pub);
+	startSimHandler(io, socket, sim_pub);
 
-		const robot_sub = nodeRobot.createSubscription(
-			"accio_interfaces/msg/Robot",
-			"robot",
-			(msg) => {
-				socket.emit("robot:state", msg);
-			}
-		);
-
-		const pending_sub = nodePending.createSubscription(
-			"accio_interfaces/msg/Orders",
-			"orders_queued",
-			(msg) => {
-				var array = [];
-				for (var i = 0; i < msg.orders.length; i++) {
-					array.push(msg.orders[i]);
-				}
-				socket.emit("orders:pending", array);
-			}
-		);
-		const fulfilled_sub = nodeFulfilled.createSubscription(
-			"accio_interfaces/msg/Orders",
-			"orders_fulfilled",
-			(msg) => {
-				var array = [];
-				for (var i = 0; i < msg.orders.length; i++) {
-					array.push(msg.orders[i]);
-				}
-				socket.emit("orders:fulfilled", array);
-			}
-		);
-		rclnodejs.spin(nodeSim);
-		rclnodejs.spin(nodeRobot);
-		rclnodejs.spin(nodePending);
-		rclnodejs.spin(nodeFulfilled);
-
-		socket.on("connect_error", (err) => {
-			console.log(`connect_error due to ${err.message}`);
-		});
-		socket.on("disconnect", () => {
-			console.log("client disconnected: ", socket.id);
-			nodeSim.destroy();
-			nodeRobot.destroy();
-			nodePending.destroy();
-			nodeFulfilled.destroy();
-			socket_connected = true;
-		});
-	// }
-
-	
+	socket.on("connect_error", (err) => {
+		console.log(`connect_error due to ${err.message}`);
+	});
+	socket.on("disconnect", () => {
+		console.log("client disconnected: ", socket.id);
+		nodeSim.destroy();
+		nodeRobot.destroy();
+		nodePending.destroy();
+		nodeFulfilled.destroy();
+	});
 });
+
 
 httpServer.listen(PORT, (err) => {
 	if (err) console.log(err);
 	console.log("Server running on Port ", PORT);
 });
+
+
+const nodeSim = new rclnodejs.Node("sim_starter");
+const nodeRobot = new rclnodejs.Node("robot_subscriber");
+const nodePending = new rclnodejs.Node("pending_subscriber");
+const nodeFulfilled = new rclnodejs.Node("fulfilled_subscriber");
+const sim_pub = nodeSim.createPublisher(
+	"std_msgs/msg/Bool",
+	"start_simulation"
+);
+
+nodeRobot.createSubscription("accio_interfaces/msg/Robot", "robot", (msg) => {
+	io.sockets.emit("robot:state", msg);
+});
+
+nodePending.createSubscription(
+	"accio_interfaces/msg/Orders",
+	"orders_queued",
+	(msg) => {
+		var array = [];
+		for (var i = 0; i < msg.orders.length; i++) {
+			array.push(msg.orders[i]);
+		}
+		io.sockets.emit("orders:pending", array);
+	}
+);
+nodeFulfilled.createSubscription(
+	"accio_interfaces/msg/Orders",
+	"orders_fulfilled",
+	(msg) => {
+		var array = [];
+		for (var i = 0; i < msg.orders.length; i++) {
+			array.push(msg.orders[i]);
+		}
+		io.sockets.emit("orders:fulfilled", array);
+	}
+);
+
+rclnodejs.spin(nodeSim);
+rclnodejs.spin(nodeRobot);
+rclnodejs.spin(nodePending);
+rclnodejs.spin(nodeFulfilled);
